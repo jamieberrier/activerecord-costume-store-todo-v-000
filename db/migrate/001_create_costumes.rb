161bdad2d3bfbb9fdbda22edd367ac14df425796
jamieberrier <berrier.jamie@gmail.com>
2019-06-12 17:11:42 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,6 +10,10 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t. :image_url
       t.timestamps
     end
   end
